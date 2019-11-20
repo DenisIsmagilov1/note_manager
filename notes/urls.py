47c674e2356aa9a94f87a17d53c741b34a961b9a
DenisIsmagilov1 <denchikism789@gmail.com>
2019-11-20 22:48:04 +0300

    Add note update view

--- notes/urls.py
+++ notes/urls.py
@@ -7,5 +7,6 @@ app_name = 'notes'
 urlpatterns = [
     path('list/', views.NoteListView.as_view(), name='note_list'),
     path('create/', views.NoteCreateView.as_view(), name='note_create'),
-    path('detail/<pk>/', views.NoteDetailView.as_view(), name='note_detail')
+    path('detail/<pk>/', views.NoteDetailView.as_view(), name='note_detail'),
+    path('update/<pk>/', views.NoteUpdateView.as_view(), name='note_update')
 ]
--- notes/views.py
+++ notes/views.py
@@ -3,7 +3,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Note
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .forms import NoteForm
 
 
@@ -32,3 +32,11 @@ class NoteDetailView(LoginRequiredMixin, DetailView):
     login_url = reverse_lazy('account:login')
     model = Note
     template_name = 'notes/detail.html'
+
+
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('account:login')
+    model = Note
+    form_class = NoteForm
+    template_name = 'notes/update.html'
+    success_url = reverse_lazy('notes:note_list')
